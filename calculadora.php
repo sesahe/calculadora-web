d0f9497335acfd92b9b49be09cbe62f20b91e609
sergi0710 <sergi0710@hotmail.com>
2024-09-25 09:23:31 +0200

    Update calculadora.php - multiplicacion/division

---
+++ calculadora.php
@@ -0,0 +1,26 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operation = $_POST['operation'];
+    $result = 0;
+
+    switch ($operation) {
+        case 'mult':
+            $result = $num1 * $num2;
+            break;
+        case 'div':
+            if($num2 != 0) {
+                $result = $num1 / $num2;
+            }else{
+                $result = "Error!";
+            }
+            break;
+        default:
+            $result = "Error!";
+            break;
+    }
+
+    echo "<p>Result is " . $result . "</p>";
+}
